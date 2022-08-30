09ce54139c0b65dd593fe75792e98cf0bde59930
Taeyoung Son <taeyoungson10@gmail.com>
2022-08-30 18:32:40 +0900

    #dev 프로젝트 생성

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Taeyoung Son on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
