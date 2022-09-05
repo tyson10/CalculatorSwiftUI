a4d272af9c38b7f25819ca28dbea565776a7fe21
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-05 22:56:13 +0900

    #PadView 생성

---
+++ CalculatorSwiftUI/PadView.swift
@@ -0,0 +1,20 @@
+//
+//  PadView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Taeyoung Son on 2022/09/05.
+//
+
+import SwiftUI
+
+struct PadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PadView_Previews: PreviewProvider {
+    static var previews: some View {
+        PadView()
+    }
+}
