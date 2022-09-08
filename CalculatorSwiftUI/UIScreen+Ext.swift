bb17d895997f34360721c0fee59a20759d4900b8
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-08 17:50:51 +0900

    #dev UIScreen+Ext 추가

---
+++ CalculatorSwiftUI/UIScreen+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  UIScreen+Ext.swift
+//  CalculatorSwiftUI
+//
+//  Created by Taeyoung Son on 2022/09/08.
+//
+
+import UIKit
+
+extension UIScreen {
+    static let size = UIScreen.main.bounds.size
+}
