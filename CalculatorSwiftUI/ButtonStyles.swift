3321ab543cf8df55de6c5c1346b20a3b53638bb9
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-08 17:51:17 +0900

    #dev NumberPadStyle 정의

--- CalculatorSwiftUI/ButtonStyles.swift
+++ CalculatorSwiftUI/ButtonStyles.swift
@@ -5,4 +5,21 @@
 //  Created by Taeyoung Son on 2022/09/06.
 //
 
-import Foundation
+import SwiftUI
+
+/// 넘버패드 버튼 스타일
+struct NumberPadStyle: ButtonStyle {
+    private var color: Color
+    
+    init(color: Color) {
+        self.color = color
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: UIScreen.size.width / 4, height: UIScreen.size.width / 4, alignment: .center)
+            .background(self.color)
+            .clipShape(Circle())
+            .font(.system(size: 14))
+    }
+}
