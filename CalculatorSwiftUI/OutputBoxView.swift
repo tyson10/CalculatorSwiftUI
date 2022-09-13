2b1d2e354d273e37e74462745124f1b58d6ce6e3
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-14 00:01:13 +0900

    #dev OutputBoxView 생성

---
+++ CalculatorSwiftUI/OutputBoxView.swift
@@ -0,0 +1,20 @@
+//
+//  OutputBoxView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Taeyoung Son on 2022/09/14.
+//
+
+import SwiftUI
+
+struct OutputBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OutputBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        OutputBoxView()
+    }
+}
