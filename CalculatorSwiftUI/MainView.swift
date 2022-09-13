ab1c926c76edcbf85fb4c85999cd1b8e6b964115
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-13 23:56:38 +0900

    #dev ContentView -> MainView 리네이밍

--- CalculatorSwiftUI/MainView.swift
+++ CalculatorSwiftUI/MainView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         PadView()
     }
@@ -15,6 +15,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
