73c5a83cfe6bf11026ded9ff0c62fa99341743ec
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-16 18:11:51 +0900

    #dev NumberPad 스택 구조 변경(0 버튼 때문)

--- CalculatorSwiftUI/PadView.swift
+++ CalculatorSwiftUI/PadView.swift
@@ -9,51 +9,57 @@ import SwiftUI
 
 struct PadView: View {
     var body: some View {
-        HStack(spacing: 0) {
-            VStack {
+        VStack {
+            HStack(spacing: 0) {
                 Button("AC", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
-                Button("7", action: {})
+                Button("+/-", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
-                Button("4", action: {})
+                Button("%", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
-                Button("1", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button("0", action: {})
+                Button("÷", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
             }
-            VStack {
-                Button("+/-", action: {})
+            
+            HStack(spacing: 0) {
+                Button("7", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
                 Button("8", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
-                Button("5", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button("2", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button("0", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-            }
-            VStack {
-                Button("%", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
                 Button("9", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button("6", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button("3", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-                Button(".", action: {})
-                    .buttonStyle(NumberPadStyle(color: .brown))
-            }
-            VStack {
-                Button("÷", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
                 Button("✕", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
-                Button("−", action: {})
+            }
+            
+            HStack(spacing: 0) {
+                Button("4", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("5", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("6", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("-", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+            }
+            
+            HStack(spacing: 0) {
+                Button("1", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("2", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("3", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
                 Button("+", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+            }
+            
+            HStack(spacing: 0) {
+                Button("0", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button("0", action: {})
+                    .buttonStyle(NumberPadStyle(color: .brown))
+                Button(".", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
                 Button("=", action: {})
                     .buttonStyle(NumberPadStyle(color: .brown))
