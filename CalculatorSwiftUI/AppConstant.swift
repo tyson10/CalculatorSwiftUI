06f0fdfd8f459cc315cf5ac2fc78973121ae91bc
Taeyoung Son <taeyoungson10@gmail.com>
2022-09-18 12:45:59 +0900

    #dev AppConstant 생성

---
+++ CalculatorSwiftUI/AppConstant.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstant.swift
+//  CalculatorSwiftUI
+//
+//  Created by Taeyoung Son on 2022/09/18.
+//
+
+import UIKit
+
+class AppConstant {
+    class UI {}
+}
+
+extension AppConstant.UI {
+    static let padSpacing: CGFloat = 20
+}
